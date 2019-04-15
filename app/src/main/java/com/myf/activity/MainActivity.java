9ff78d849acfd1e3b38c6acd3e10196bfb67f9ea
mayuefei <yuefei165524@163.com>
2019-04-15 16:32:36 +0800

    打包设置

--- app/src/main/java/com/myf/activity/MainActivity.java
+++ app/src/main/java/com/myf/activity/MainActivity.java
@@ -1,16 +1,9 @@
 package com.myf.activity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.myf.R;
 import com.myf.base.BaseActivity;
-import com.myf.okhttp.OkHttpApi;
-import com.myf.util.LogUtil;
-import com.myf.util.ToastUtil;
-import com.zhy.http.okhttp.callback.StringCallback;
-
-import okhttp3.Call;
 
 public class MainActivity extends BaseActivity {
 
