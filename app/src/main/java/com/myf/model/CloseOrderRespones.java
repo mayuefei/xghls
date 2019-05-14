62a83ab7b45f488061786a1431be7131e0a343fa
郭红 <18636406523@163.com>
2019-05-14 11:59:20 +0800

    关闭订单接口业务bean编写

--- app/src/main/java/com/myf/model/CloseOrderRespones.java
+++ app/src/main/java/com/myf/model/CloseOrderRespones.java
@@ -2,4 +2,13 @@ package com.myf.model;
 
 public class CloseOrderRespones {
 
+    /**
+     * code : 1
+     * msg : 修改成功!
+     * data :
+     */
+
+    public String code;
+    public String msg;
+    public String data;
 }
